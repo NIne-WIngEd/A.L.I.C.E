548b23e34b0f205ab3969c44c5e47bd3b87e7ec4
NIne-WIngEd <rayan.ce2028@gmail.com>
2026-09-23 17:21:54 -0500

    test(n0): include runtime authorization in checkpoint lineage

--- tests/eipm/test_n0_full_envelope_joint_runtime_v1.py
+++ tests/eipm/test_n0_full_envelope_joint_runtime_v1.py
@@ -695,6 +695,7 @@ def test_successor_trainer_dev_evaluator_and_checkpoint_contract_exist_before_gr
         "teacher_audit_sha256",
         "static_proof_receipt_sha256",
         "operator_evidence_token_receipt_sha256",
+        "training_authorization_sha256",
         "accelerator_state_tree_sha256",
         "full_system_sha256",
         "objective_state_sha256",
